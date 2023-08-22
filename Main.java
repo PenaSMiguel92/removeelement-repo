c367a55d8877c54b8e67d46002db739edbcdceb9
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-21 19:58:14 -0700

    solution found

--- Main.java
+++ Main.java
@@ -8,8 +8,8 @@ public class Main {
         int[] array2 = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
         int[] array3 = new int[] { 0, 1, 4, 7, 7, 8, 7, 10 };
 
-        System.out.println(Arrays.toString(array1) + " " + sol.removeElement(array1, 3));
-        System.out.println(Arrays.toString(array2) + " " + sol.removeElement(array2, 2));
-         System.out.println(Arrays.toString(array3) + " " + sol.removeElement(array3, 7));
+        System.out.println(sol.removeElement(array1, 3) + " " + Arrays.toString(array1));
+        System.out.println(sol.removeElement(array2, 2) + " " + Arrays.toString(array2));
+         System.out.println(sol.removeElement(array3, 7) + " " + Arrays.toString(array3));
     } 
 }
--- Solution.java
+++ Solution.java
@@ -1,5 +1,27 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                count++;
+                continue;
+            }
+
+            if (nums[i] == val) {
+                int j = 0;
+                while (i + j < nums.length && nums[i + j] == val) {
+                    j++;
+                }
+                if (i + j >= nums.length) {
+                    break;
+                }
+
+                int repval = nums[i];
+                nums[i] = nums[i + j];
+                nums[i + j] = repval;
+                count++;
+            }
+        }
+        return count;
     }
 }
